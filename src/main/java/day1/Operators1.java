4716e2ea34c3fd119a1db1ea0b109dd75641f848
mishranaveen677 <mishranaveen677@gmail.com>
2025-01-17 00:02:20 +0530

    first commit

---
+++ src/main/java/day1/Operators1.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Operators1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        int sum = x + y;
+
+        System.out.println("sum = " + sum);
+
+
+        double sub = y - x;
+
+
+
+    }
+}
